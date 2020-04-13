9c5d9d65da26149ca17b94d142e4613e59c10067
mrsmarah <marahabdallah91@gmail.com>
2020-04-14 01:54:45 +0300

    gg

---
+++ data/books.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS books;
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  image_url VARCHAR(2048),
+  title VARCHAR(255),
+  author VARCHAR(255),
+  description TEXT
+)
--- server.js
+++ server.js
@@ -4,17 +4,66 @@
 
 require('dotenv').config();
 const express = require('express');
+const pg = require('pg');
 const superagent = require('superagent');
 const PORT = process.env.PORT || 4000;
 const app = express();
+const client = new pg.Client(process.env.DATABASE_URL);
+client.on('error', (err) => console.log(err));
+
+//middlewares
+
 app.use(express.urlencoded({ extended: true })); //Tell the server I want to post a FORM
-app.use('/public', express.static('public'));
-app.set('view engine', 'ejs'); // set the view engine
+app.use('/public', express.static('public')); // tell the server to connect the CSS
+
+// set the view engine
+
+app.set('view engine', 'ejs'); 
 
 //Main Route
 
-app.get('/', (req, res) => {  // render the index.ejs from the views folder
-  res.render('pages/index.ejs');
+app.get('/', (req, res) => {  // render the index.ejs from DB
+  const SQL = 'SELECT * FROM books;';
+  client
+    .query(SQL)
+    .then((results) => {
+      res.render('pages/index.ejs', { book: results.rows });
+    })
+    .catch((err) => {
+      errorHandler(err, req, res);
+    });
+});
+
+app.get('/books/:id', (req, res) => {  // render the 
+  const SQL = 'SELECT * FROM books WHERE id=$1;';
+  const values = [req.params.id];
+  client
+    .query(SQL, values)
+    .then((results) => {
+      res.render('pages/books/detail.ejs', { book : results.rows[0] });
+    })
+    .catch((err) => {
+      errorHandler(err, req, res);
+    });
+});
+
+app.post('/books',(req, res)=>{
+  const sqlSearch = 'SELECT title FROM books WHERE title=$1;'
+  const searchVal = [req.body.title];
+  client.query(sqlSearch, searchVal).then((searchedResult)=> {
+    if(searchedResult.rows.length > 0){
+        res.redirect('/');
+    }else{
+  const SQL ='INSERT INTO books (image_url,title,author,description) VALUES ($1,$2,$3,$4)';
+  const values =[req.body.img, req.body.title ,req.body.author ,req.body.description];
+  client.query(SQL, values).then((results) => {
+      // res.render('pages/books/detail.ejs', { book : results.rows[0] });
+      res.redirect('/');
+    })
+    .catch((err) => {
+      errorHandler(err, req, res);
+    })
+}})
 });
 
 //Form Route
@@ -61,7 +110,6 @@ function errorHandler(err, req, res) {
   res.status(500).send(err);
 }
 
-app.listen(PORT, () =>
-      console.log(`my server is up and running on port ${PORT}`)
-    );
-    
+client.connect().then(() => {
+  app.listen(PORT, () => console.log(`my server is up and running on port ${PORT}`));
+});
